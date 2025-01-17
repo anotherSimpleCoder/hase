eeef43f4f08fc8bd830376a94b3bb1b72464310b
Gr8Sophia <sophiasarkhovska@gmail.com>
2025-01-17 15:38:01 +0100

    Changes

--- backend/src/main/java/de/hase/hasev2/Hasev2Application.java
+++ backend/src/main/java/de/hase/hasev2/Hasev2Application.java
@@ -3,6 +3,8 @@ package de.hase.hasev2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Hasev2Application {
 
