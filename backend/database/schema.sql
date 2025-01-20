9e7c413a5afeb0932706d00898d9c8680d068748
ErwinHlz <erwinholzhauser.eh@gmail.com>
2025-01-20 12:09:21 +0100

    updated database

--- backend/database/schema.sql
+++
@@ -1,8 +0,0 @@
-create table Appointments (
-    appointmentId integer autoincrement,
-    name varchar(50) not null,
-    date datetime not null,
-    location varchar(50),
-
-    primary key (appointmentId)
-);
