99c3772f55d73e286d1292563cf24e54caa693e9
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-01-31 13:31:23 +0100

    Added missing password field into schema file.

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Users (
-                       matrikelNr INTEGER PRIMARY KEY AUTOINCREMENT ,
-                       firstName VARCHAR(50) NOT NULL,
-                       lastName VARCHAR(50) NOT NULL ,
-                       email VARCHAR(50) NOT NULL);
+    matrikelNr INTEGER PRIMARY KEY AUTOINCREMENT ,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL ,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
 
 INSERT INTO Users(firstName, lastName, email)
 VALUES ('Erwin', 'Holzhauser','erwinholzhauser.eh@gmail.com');
@@ -11,5 +13,5 @@ CREATE TABLE Appointments (
         appointmentId INTEGER PRIMARY KEY AUTOINCREMENT ,
         name VARCHAR(50) NOT NULL,
         date LocalDateTime NOT NULL,
-         location VARCHAR(50)
+        location VARCHAR(50)
                           );
