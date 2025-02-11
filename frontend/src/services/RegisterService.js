ca828947c420aca953a823f5db3ed527c650752c
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-02-11 16:34:09 +0100

    Implemented error/success handling for login service.

--- frontend/src/services/RegisterService.js
+++ frontend/src/services/RegisterService.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 export default {
   async register(user) {
-
-
     const response = await axios.post('http://localhost:8080/users', user)
 
     if (response.status !== 200) {
