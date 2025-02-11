26929863ab776404d2b517e57dba1551bf9acc39
Hanan Ahmed Ashir <hananashir098@gmail.com>
2025-02-11 17:24:43 +0100

    Added Methods to Map Users to Appointments and vice versa

---
+++ backend/src/main/java/de/hase/hasev2/participates/participatesService.java
@@ -0,0 +1,52 @@
+package de.hase.hasev2.participates;
+import de.hase.hasev2.appointment.Appointment;
+import de.hase.hasev2.config.HikariService;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static de.hase.hasev2.database.Tables.APPOINTMENTS;
+import static de.hase.hasev2.database.Tables.PARTICIPATES;
+
+@Service
+public class participatesService {
+    private Logger logger;
+    private DSLContext database;
+
+    public participatesService(@Autowired HikariService hikariService){
+        this.logger = LoggerFactory.getLogger(participatesService.class);
+        try{
+            database = DSL.using(hikariService.getDataSource().getConnection());
+        } catch(SQLException e){
+            this.logger.error("Database Error: "+ e.getMessage());
+        }
+    }
+
+    public List<Appointment> getAppointmentsForUser(int matrikelNr){
+       return database.select(APPOINTMENTS.APPOINTMENTID, APPOINTMENTS.NAME, APPOINTMENTS.DATE, APPOINTMENTS.LOCATION)
+               .from(PARTICIPATES.naturalJoin(APPOINTMENTS))
+               .where(PARTICIPATES.MATRIKELNR.eq(matrikelNr))
+               .fetchInto(Appointment.class);
+    }
+
+    public void addUserToAppointments(int appointmentId, List<Integer> matrikelNrs ){
+        for(int matrikelNr : matrikelNrs){
+            database.insertInto(PARTICIPATES, PARTICIPATES.APPOINTMENTID, PARTICIPATES.MATRIKELNR)
+                    .values(appointmentId, matrikelNr)
+                    .execute();
+        }
+    }
+    public void removeUsersFromAppointment(int appointmentId, List<Integer> matrikelNrs){
+        for(int matrikelNr : matrikelNrs){
+            database.deleteFrom(PARTICIPATES)
+                    .where(PARTICIPATES.MATRIKELNR.eq(matrikelNr).and(PARTICIPATES.APPOINTMENTID.eq(appointmentId)))
+                    .execute();
+        }
+    }
+}
