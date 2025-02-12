e708ec06a839851cecd2c298c30cb8f787c31885
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-02-12 11:04:30 +0100

    Fixed spelling mistake in ParticipatesService.java and had to implement foreign key constraint check on backend since jOOQ doesn't enforce strict foreign key integrity.

--- backend/src/main/java/de/hase/hasev2/participates/ParticipatesController.java
+++ backend/src/main/java/de/hase/hasev2/participates/ParticipatesController.java
@@ -1,11 +1,11 @@
 package de.hase.hasev2.participates;
 
 import de.hase.hasev2.appointment.Appointment;
-import de.hase.hasev2.database.tables.Appointments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -16,7 +16,7 @@ import java.util.Map;
 @CrossOrigin
 public class ParticipatesController {
     @Autowired
-    private participatesService participatesService;
+    private ParticipatesService participatesService;
 
     @GetMapping("")
     public ResponseEntity<List<Appointment>> getAppointmentForUsers(@RequestParam("user") int matrikelNr){
@@ -27,13 +27,21 @@ public class ParticipatesController {
 
     @PostMapping("")
     public ResponseEntity<Boolean> addUsersToAppointments(@RequestBody Map<Integer, Integer> appointmentsToUsersMap){
-        participatesService.addUsersToAppointments(appointmentsToUsersMap);
-        return ResponseEntity.ok(true);
+        try {
+            participatesService.addUsersToAppointments(appointmentsToUsersMap);
+            return ResponseEntity.ok(true);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("")
     public ResponseEntity<Boolean> removeUsersFromAppointments(@RequestBody Map<Integer, Integer> appointmentsToUsersMap){
-        participatesService.removeUsersFromAppointments(appointmentsToUsersMap);
-        return ResponseEntity.ok(true);
+        try {
+            participatesService.removeUsersFromAppointments(appointmentsToUsersMap);
+            return ResponseEntity.ok(true);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
---
+++ backend/src/main/java/de/hase/hasev2/participates/ParticipatesService.java
@@ -0,0 +1,100 @@
+package de.hase.hasev2.participates;
+import de.hase.hasev2.appointment.Appointment;
+import de.hase.hasev2.appointment.AppointmentService;
+import de.hase.hasev2.config.HikariService;
+import de.hase.hasev2.user.User;
+import de.hase.hasev2.user.UserService;
+import org.jooq.DSLContext;
+import org.jooq.conf.ParamType;
+import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DSL;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import static de.hase.hasev2.database.Tables.*;
+
+@Service
+public class ParticipatesService {
+    private AppointmentService appointmentService;
+    private UserService userService;
+
+    private Logger logger;
+    private DSLContext database;
+
+    public ParticipatesService(@Autowired HikariService hikariService, @Autowired AppointmentService appointmentService, @Autowired UserService userService) {
+        this.logger = LoggerFactory.getLogger(ParticipatesService.class);
+
+        this.appointmentService = appointmentService;
+        this.userService = userService;
+
+        try (var connection = hikariService.getDataSource().getConnection()){
+            connection.createStatement().execute("PRAGMA foreign_keys=ON");
+
+            database = DSL.using(hikariService.getDataSource().getConnection());
+        } catch(SQLException e){
+            this.logger.error("Database Error: "+ e.getMessage());
+        }
+    }
+
+    private void validMapping(Map<Integer, Integer> appointmentsToUsersMap) throws Exception {
+        for(int appointmentId : appointmentsToUsersMap.keySet()){
+            this.appointmentService.findAppointment(appointmentId)
+                    .orElseThrow(() -> new Exception("Invalid appointment"));
+
+            for (int matrikelNr : appointmentsToUsersMap.values()){
+                this.userService.findUser(matrikelNr)
+                                .orElseThrow(() -> new Exception("Invalid user"));
+            }
+        }
+    }
+
+    public List<Appointment> getAppointmentsForUser(int matrikelNr){
+       return database.select(APPOINTMENTS.APPOINTMENTID, APPOINTMENTS.NAME, APPOINTMENTS.DATE, APPOINTMENTS.LOCATION)
+               .from(PARTICIPATES.naturalJoin(APPOINTMENTS))
+               .where(PARTICIPATES.MATRIKELNR.eq(matrikelNr))
+               .fetchInto(Appointment.class);
+    }
+
+    public List<User> getUsersForAppointment(int appointmentId){
+        return database.select(USERS.MATRIKELNR, USERS.FIRSTNAME, USERS.LASTNAME, USERS.EMAIL, USERS.PASSWORD)
+                .from(PARTICIPATES.naturalJoin(USERS))
+                .where(PARTICIPATES.APPOINTMENTID.eq(appointmentId))
+                .fetchInto(User.class);
+    }
+
+    public void addUsersToAppointments(Map<Integer, Integer> appointmentsToUsersMap) throws Exception {
+        validMapping(appointmentsToUsersMap);
+
+        for(int appointmentId : appointmentsToUsersMap.keySet()){
+            for (int matrikelNr : appointmentsToUsersMap.values()){
+                database.insertInto(PARTICIPATES, PARTICIPATES.APPOINTMENTID, PARTICIPATES.MATRIKELNR)
+                        .values(appointmentId, matrikelNr)
+                        .execute();
+            }
+        }
+    }
+
+    public void removeUsersFromAppointments(Map<Integer, Integer> appointmentsToUsersMap) throws Exception {
+        validMapping(appointmentsToUsersMap);
+
+        for (int appointmentId : appointmentsToUsersMap.keySet()){
+            for (int matrikelNr : appointmentsToUsersMap.values()){
+                database.deleteFrom(PARTICIPATES)
+                        .where(PARTICIPATES.MATRIKELNR.eq(matrikelNr).and(PARTICIPATES.APPOINTMENTID.eq(appointmentId)))
+                        .returningResult()
+                        .fetch();
+            }
+        }
+    }
+
+    public void deleteAllMappings() {
+        database.deleteFrom(PARTICIPATES)
+                .execute();
+    }
+}
