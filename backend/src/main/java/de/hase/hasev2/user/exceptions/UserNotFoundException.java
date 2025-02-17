ec475b0c83ada67d4f29ab6ad73f6e362f9a8aac
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-02-17 11:14:34 +0100

    Changed Exception type of UserNotFoundException to a catchable Exception instead of a runtime exception.

--- backend/src/main/java/de/hase/hasev2/user/exceptions/UserNotFoundException.java
+++ backend/src/main/java/de/hase/hasev2/user/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package de.hase.hasev2.user.exceptions;
 
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends Exception {
     public UserNotFoundException(int matrikelNr) {
         super("User with Matrikelnummer " + matrikelNr + " not found");
     }
