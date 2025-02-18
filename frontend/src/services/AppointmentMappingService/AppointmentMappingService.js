42b3d2eb55558d7e06bdc44039e4ec054eed8e20
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-02-18 11:13:29 +0100

    Added error handling into AppointmentMappingService.

--- frontend/src/services/AppointmentMappingService/AppointmentMappingService.js
+++ frontend/src/services/AppointmentMappingService/AppointmentMappingService.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 const API_URL = 'http://localhost:8080'
 
 export default {
-  async getAppointmentforUser(mapData) {
+  async postAppointmentforUser(mapData) {
+    if(!mapData) {
+      throw new Error('Mapping is required')
+    }
+
     await axios.post(`${API_URL}/user-appointment`, mapData, {
       headers: {
         'Content-Type': 'application/json',
@@ -12,12 +16,20 @@ export default {
   },
 
   async getAppointmentsForUser(matrikelNr) {
+    if(!matrikelNr) {
+      throw new Error('Matrikelnummer is required')
+    }
+
     const response = await axios.get(`${API_URL}/user-appointment?matrikelNr=${matrikelNr}`)
 
     return await response
   },
 
   async removeAppointmentFromUser(mapData) {
+    if(!mapData) {
+      throw new Error('Mapping is required')
+    }
+
     console.log(mapData)
     await axios.delete(
       `${API_URL}/user-appointment`,
---
+++ frontend/src/services/AppointmentMappingService/AppointmentMappingService.spec.js
@@ -0,0 +1,20 @@
+import {test, expect} from 'vitest'
+import AppointmentMappingService from './AppointmentMappingService'
+
+test('post invalid mapping should throw error', async () => {
+  await expect((async () => {
+    await AppointmentMappingService.postAppointmentforUser()
+  })()).rejects.toThrowError('Mapping is required!')
+})
+
+test('get appointment for empty Matrikelnummer should throw error', async () => {
+  await expect((async () => {
+    await AppointmentMappingService.getAppointmentsForUser()
+  })()).rejects.toThrowError('Matrikelnummer is required')
+})
+
+test('remove invalid mapping should throw error', async () => {
+  await expect((async () => {
+    await AppointmentMappingService.postAppointmentforUser()
+  })()).rejects.toThrowError('Mapping is required')
+})
