2b0c78ebc06cbf71f8ad5f8abb1c367e0baaa1d9
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-02-18 11:18:48 +0100

    Fixed assertion in AppointmentMappingService unit test

--- frontend/src/services/AppointmentMappingService/AppointmentMappingService.spec.js
+++ frontend/src/services/AppointmentMappingService/AppointmentMappingService.spec.js
@@ -4,7 +4,7 @@ import AppointmentMappingService from './AppointmentMappingService'
 test('post invalid mapping should throw error', async () => {
   await expect((async () => {
     await AppointmentMappingService.postAppointmentforUser()
-  })()).rejects.toThrowError('Mapping is required!')
+  })()).rejects.toThrowError('Mapping is required')
 })
 
 test('get appointment for empty Matrikelnummer should throw error', async () => {
