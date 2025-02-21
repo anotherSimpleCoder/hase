ada88e1f4e1e41740793eaa647115f59ec9bd043
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-02-21 16:20:57 +0100

    Introduced locked routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AuthService from '@/services/AuthService/AuthService.js'
+
 import AppointmentView from '@/views/AppointmentView.vue'
 import UserView from '@/views/UserView.vue'
 import HomeView from '@/views/HomeView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
 import UserAppointmentView from '@/views/UserAppointmentView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,16 +20,43 @@ const router = createRouter({
       path: '/users',
       name: 'User',
       component: UserView,
+      beforeEnter: (to, from, next) => {
+        if(AuthService.isLoggedIn()) {
+          next()
+        } else {
+          next({
+            name: 'Login'
+          })
+        }
+      }
     },
     {
       path: '/appointments',
       name: 'Appointments',
       component: AppointmentView,
+      beforeEnter: (to, from, next) => {
+        if(AuthService.isLoggedIn()) {
+          next()
+        } else {
+          next({
+            path: '/login'
+          })
+        }
+      }
     },
     {
       path: '/my-appointments',
       name: "My Appointments",
-      component: UserAppointmentView
+      component: UserAppointmentView,
+      beforeEnter: (to, from, next) => {
+        if(AuthService.isLoggedIn()) {
+          next()
+        } else {
+          next({
+            name: 'Login'
+          })
+        }
+      }
     },
     {
       path: '/register',
